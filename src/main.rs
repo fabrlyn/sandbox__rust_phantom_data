3fbe066cfa52b2b1c1a27b45a48302b6c4de30f4
fabrlyn <robert.barlin@gmail.com>
2020-08-15 10:18:00 +0200

    More frequent reads

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use std::fs::read_to_string;
 use std::fs::write;
 use std::marker::PhantomData;
 use std::path::PathBuf;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
 use std::sync::Arc;
-use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
 pub struct Reader {
     path: PathBuf,
     handle: Option<thread::JoinHandle<()>>,
-    keep_reading: Arc<Mutex<bool>>,
+    keep_reading: Arc<AtomicBool>,
 }
 
 pub struct Writer<'a> {
@@ -27,10 +28,7 @@ impl Writer<'_> {
 impl Drop for Reader {
     fn drop(&mut self) {
         println!("Dropping Reader");
-        {
-            let mut keep_reading = self.keep_reading.lock().unwrap();
-            *keep_reading = false;
-        }
+        self.keep_reading.store(false, Ordering::Relaxed);
 
         if let Some(handle) = self.handle.take() {
             if let Err(e) = handle.join() {
@@ -45,7 +43,7 @@ impl Reader {
         Reader {
             path,
             handle: None,
-            keep_reading: Arc::new(Mutex::new(true)),
+            keep_reading: Arc::new(AtomicBool::new(true)),
         }
     }
 
@@ -60,11 +58,13 @@ impl Reader {
         let keep_reading = self.keep_reading.clone();
         let path = self.path.clone();
 
-        let handle = thread::spawn(move || {
-            while *keep_reading.lock().unwrap() {
-                thread::sleep(Duration::from_secs(1));
-                let content = read_to_string(&path);
-                println!("Content is: {:?}", content);
+        let handle = thread::spawn(move || loop {
+            thread::sleep(Duration::from_millis(100));
+            let content = read_to_string(&path);
+            println!("Content is: {:?}", content);
+
+            if !keep_reading.load(Ordering::Relaxed) {
+                break;
             }
         });
         self.handle = Some(handle)
@@ -74,17 +74,18 @@ impl Reader {
 fn main() {
     let path = PathBuf::from("./file-to-write-to.txt");
 
+    let mut writer = None;
     let mut reader = Reader::new(path);
     reader.start();
-    let writer = Some(reader.create_writer());
+    writer = Some(reader.create_writer());
 
     if let Some(writer) = writer {
         println!("Writing first value");
-        writer.write("Hello".to_string());
-        thread::sleep(Duration::from_millis(1500));
+        writer.write("First Value".to_string());
+        thread::sleep(Duration::from_millis(400));
 
         println!("Writing second value");
-        writer.write("Good bye".to_string());
-        thread::sleep(Duration::from_millis(1500));
+        writer.write("Second Value".to_string());
+        thread::sleep(Duration::from_millis(400));
     }
 }
